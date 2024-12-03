3d5e441611f45de7f3c3a0703a6e6548da5f1506
16787 <camtedder10@gmail.com>
2024-12-03 14:12:25 -0500

    Add game selection page

---
+++ public/gameselectionjs.js
@@ -0,0 +1,7 @@
+const featuredGameCard = document.getElementById("featured-game");
+
+// Add a click event listener to the featured game
+featuredGameCard.addEventListener("click", () => {
+  // Redirect to the game page (you can implement this page later)
+  window.location.href = "gamepage.html";
+});
